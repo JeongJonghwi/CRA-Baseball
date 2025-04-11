f0b81ee235e9c50d0bdc7944d98738d53734b456
Jonghwi <ellia2431@naver.com>
2025-04-11 16:31:56 +0900

    [feature] p30

--- Baseball KATA/baseball_test.cpp
+++ Baseball KATA/baseball_test.cpp
@@ -6,6 +6,11 @@ TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
 	EXPECT_THROW(game.guess(string("12")), length_error);
 }
 
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+}
+
 int main(void) {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
